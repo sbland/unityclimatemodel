88a43dbb6a9651ada2f338d1f16f37f5f8e0a84c
snbland_Lap <sbland.co.uk@gmail.com>
2021-04-13 20:24:16 +0100

    Initial unity commit

---
+++ Assets/scripts/Cell.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Weather
+{
+    public float precipitation;
+    public float wind_speed;
+    public float wind_direction;
+    public float cloud_cover;
+
+    public Weather(
+        float _precipitation,
+        float _wind_speed,
+        float _wind_direction,
+        float _cloud_cover
+        )
+    {
+        precipitation = _precipitation;
+        wind_speed = _wind_speed;
+        wind_direction = _wind_direction;
+        cloud_cover = _cloud_cover;
+    }
+}
+
+
+
+public class Cell : MonoBehaviour
+{
+    public Weather weather;
+    // Start is called before the first frame update
+
+    public float get_cell_influence(float wind_direction, float wind_speed, float angle)
+    {
+        // Debug.Log("dir " + wind_direction + " angle " + angle);
+        float diff = 1 - System.Math.Abs(((wind_direction - angle) % 360) / 180);
+        return 0.001f * wind_speed * diff;
+    }
+
+    public Weather influenceWeather(Weather localWeather, Weather targetWeather, float influence)
+    {
+        float precipitation = localWeather.precipitation + (targetWeather.precipitation - localWeather.precipitation) * influence;
+        float wind_speed = localWeather.wind_speed + (targetWeather.wind_speed - localWeather.wind_speed) * influence;
+        float wind_direction = localWeather.wind_direction; // + (targetWeather.wind_direction - localWeather.wind_direction) * influence;
+        float cloud_cover = localWeather.cloud_cover; // + (targetWeather.cloud_cover - localWeather.cloud_cover) * influence;
+        return new Weather(
+            precipitation,
+            wind_speed,
+            wind_direction,
+            cloud_cover
+        );
+    }
+
+    public void update_cell(Cell[] neighbours)
+    {
+        foreach (Cell n in neighbours)
+        {
+            Vector3 targetDir = n.gameObject.transform.position - transform.position;
+            // TODO: Check angle
+            float angle = Vector3.SignedAngle(targetDir, Vector3.forward, Vector3.up);
+            float influence = get_cell_influence(n.weather.wind_direction, n.weather.wind_speed, angle);
+            Weather newWeather = influenceWeather(weather, n.weather, influence);
+            weather = newWeather;
+        }
+    }
+    void Start()
+    {
+        weather = new Weather(
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/CellManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CellManager : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public int cell_width;
+    public int cell_height;
+    public Cell[,] cells;
+
+    public int[,][][] neighbours;
+    public GameObject cell_template;
+
+    public int[][] get_neighbours(int x, int y, int width, int height)
+    {
+        int[][] locs = new int[8][] { new int[2] { -1, -1 }, new int[2] { 0, -1 }, new int[2] { 1, -1 }, new int[2] { -1, 0 }, new int[2] { 1, 0 }, new int[2] { -1, 1 }, new int[2] { 0, 1 }, new int[2] { 1, 1 } };
+        int[][] n = new int[8][];
+
+        int count = 0;
+        foreach (int[] loc in locs)
+        {
+            int new_x = x + loc[0];
+            int new_y = y + loc[1];
+
+            if ((new_x >= 0) && (new_x < width) && (new_y >= 0) && (new_y < height))
+            {
+                n[count] = new int[2] { new_x, new_y };
+                count += 1;
+            }
+        }
+
+        int[][] n_out = new int[count][];
+        System.Array.Copy(n, n_out, count);
+        Debug.Log("n count " + count);
+        return n_out;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cells = new Cell[width, height];
+        neighbours = new int[width, height][][];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject objToSpawn = GameObject.Instantiate(cell_template);
+                objToSpawn.name = "Cell(" + x + "," + y + ")";
+                int xpos = x * cell_width;
+                int ypos = y * cell_height;
+                objToSpawn.transform.position = new Vector3(xpos, 0, ypos);
+                Cell new_cell = objToSpawn.AddComponent(typeof(Cell)) as Cell;
+                cells[x, y] = new_cell;
+                neighbours[x, y] = get_neighbours(x, y, width, height);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // Debug.Log("================ cell " + x + " " + y);
+                Cell cell = cells[x, y];
+                int[][] neighbour_i = neighbours[x, y];
+                int n_count = neighbour_i.Length;
+                Cell[] cell_neighbours = new Cell[n_count];
+
+                for (int i = 0; i < n_count; i++)
+                {
+                    cell_neighbours[i] = cells[neighbour_i[i][0], neighbour_i[i][1]];
+                }
+                cell.update_cell(cell_neighbours);
+            }
+        }
+    }
+}
